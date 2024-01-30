b8e095c2283d168526daeed636d64e3b67b6a38a
shdsgm <125741023+shdsgm@users.noreply.github.com>
2024-01-30 16:42:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,36 @@ from aiogram import Bot, Dispatcher, types
 import g4f
 from aiogram.filters.command import Command
 import asyncio
-# from curl_cffi import requests
+import sqlite3
 
 # Включите логирование
 logging.basicConfig(level=logging.INFO)
 
 # Инициализация бота
-API_TOKEN = '6477962823:AAFr2SHtUCn6ZYcp8q2IVxLSQxgxGcm_hq4'
+API_TOKEN = 'YOUR TOKEN HERE'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
 # Словарь для хранения истории разговоров
 conversation_history = {}
+
+# Initialize SQLite database and create a table if not exists
+conn = sqlite3.connect('conversation_history.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS conversation_history
+             (user_id INTEGER, role TEXT, content TEXT)''')
+conn.commit()
+
+# Function to save message to SQLite database
+def save_to_database(user_id, user_role, user_content, assistant_role, assistant_content):
+    c.execute("INSERT INTO conversation_history (user_id, role, content) VALUES (?, ?, ?), (?, ?, ?)",
+              (user_id, user_role, user_content, user_id, assistant_role, assistant_content))
+    conn.commit()
+
+
+def get_conversation_history(user_id):
+    c.execute("SELECT role, content FROM conversation_history WHERE user_id=?", (user_id,))
+    return c.fetchall()
 
 # Функция для обрезки истории разговора
 def trim_history(history, max_length=4096):
@@ -23,6 +41,7 @@ def trim_history(history, max_length=4096):
         removed_message = history.pop(0)
         current_length -= len(removed_message["content"])
     return history
+
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
@@ -65,19 +84,21 @@ async def send_welcome(message: types.Message):
         response = await g4f.ChatCompletion.create_async(
             model=g4f.models.default,
             messages=chat_history,
-            provider=g4f.Provider.You,
+            provider=g4f.Provider.ChatBase,
         )
         chat_gpt_response = response
     except Exception as e:
-        print(f"{g4f.Provider.You.__name__}:", e)
+        print(f"{g4f.Provider.ChatBase.__name__}:", e)
         chat_gpt_response = "Sorry. Error occurred. Please try again or later. "
+
+    # Save both user message and assistant response to SQLite database
+    save_to_database(user_id, "user", user_input, "assistant", chat_gpt_response)
 
     conversation_history[user_id].append({"role": "assistant", "content": chat_gpt_response})
     print(conversation_history)
     length = sum(len(message["content"]) for message in conversation_history[user_id])
     print(length)
     await message.answer(chat_gpt_response)
-
 
 async def main():
     await dp.start_polling(bot)
